82202febbcd3bb13db970ce48da31c3e46c0b02d
Cristian <cristiancaraballo112@gmail.com>
2021-04-17 21:39:31 -0400

    refactorizando api-v2

--- index.js
+++ index.js
@@ -4,15 +4,16 @@ const app = express();
 const cors = require('cors');
 
 const router = require('./routes/router');
-
-app.use(cors({
-  origin: 'https://cris-names.vercel.app',
-}));
+app.use(
+  cors({
+    origin: process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:8080' : 'https://cris-names.vercel.app',
+  }),
+);
 
 app.use(express.json());
 
 router(app);
 
 app.listen(3000, () => {
-  console.log('Server listening in http://localhost:3000');
+  console.log('Server listening in http://localhost:3000 \nENV: ' + process.env.NODE_ENV);
 });
